2e4f4cf313cdfeb6616ee9a17cad8a75770e8d5b
Adam Nowaczyk <adam.nowaczyk@acaisoft.com>
2015-03-05 22:24:07 +0100

    Organization webpage

---
+++ README.md
@@ -0,0 +1,6 @@
+# Acaisoft
+
+Acaisoft is a modern development company. We focus on enterprise web application development. We implement systems build across all tiers, from database, 
+through backend/middleware to frontend, always with top quality. We are up to date with modern tools, so we are able to create testable and maintainable 
+enterprise applications. We always design web applications to be responsive, so they are accessible on all devices (mobiles, tablets, desktops). <strong>We 
+love what we do</strong> and we love to see how <strong>our customer's business grows</strong> thanks to our work.
---
+++ js/main.js
@@ -0,0 +1,47 @@
+var offset = -120;
+if (Modernizr.mq('(max-width: 768px)')) {
+  offset = -50;
+}
+
+$.localScroll({hash: true, offset: offset});
+
+$('body').scrollspy({target: '.navbar-collapse', offset: -offset+10});
+
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+	var mapOptions = {
+	  zoom: 11,
+	  center: new google.maps.LatLng(52.1575056, 21.0731831),
+	  styles: [
+		{
+		  "featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]
+		},
+		{
+		  "featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]
+		}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {
+		  "featureType": "road.arterial",
+		  "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]
+		}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {
+		  "featureType": "transit",
+		  "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+		}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {
+		  "featureType": "water",
+		  "elementType": "labels",
+		  "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]
+		}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}]
+	};
+
+	var mapElement = document.getElementById('map');
+
+	// Create the Google Map using our element and options defined above
+	var map = new google.maps.Map(mapElement, mapOptions);
+
+	// Let's also add a marker while we're at it
+	var marker = new google.maps.Marker({
+	  position: new google.maps.LatLng(52.1575056, 21.0731831),
+	  map: map,
+	  title: 'Snazzy!'
+	});
+}
